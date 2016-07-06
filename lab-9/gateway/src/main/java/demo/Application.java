04e13ea27f59b4ab12c492d00bf5d2cf90d516b2
Eric Leung <eric.leung@alumni.utoronto.ca>
2016-07-06 00:35:20 -0400

    Lab 9: Zuul gateway

--- lab-9/gateway/src/main/java/demo/Application.java
+++ lab-9/gateway/src/main/java/demo/Application.java
@@ -1,12 +1,32 @@
 package demo;
 
+import javax.servlet.Filter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
-public class Application {
+@EnableZuulProxy
+public class Application
+{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Application.class, args);
+  }
+
+  /**
+   * Use shallow etag to eliminate the need to send a payload to the client when
+   * nothing has changed
+   * 
+   * @return
+   */
+  @Bean
+  public Filter shallowEtagHeaderFilter()
+  {
+    return new ShallowEtagHeaderFilter();
+  }
 }
